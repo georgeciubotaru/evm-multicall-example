08edb8495a700ebe017c28a3aa7f5fe64be86d85
George Ciubotaru <geeku96@gmail.com>
2024-02-15 09:53:50 +0200

    feat: update processor settings

--- src/processor.ts
+++ src/processor.ts
@@ -1,4 +1,4 @@
-import {lookupArchive} from '@subsquid/archive-registry'
+import { lookupArchive } from '@subsquid/archive-registry'
 import {
     BlockHeader,
     DataHandlerContext,
@@ -7,20 +7,24 @@ import {
     Log as _Log,
     Transaction as _Transaction,
 } from '@subsquid/evm-processor'
-import {Store} from '@subsquid/typeorm-store'
+import { Store } from '@subsquid/typeorm-store'
 import * as erc721 from './abi/erc721'
 
 export const CONTRACT_ADDRESS = '0xac5c7493036de60e63eb81c5e9a440b42f47ebf5'
-export const MULTICALL_ADDRESS = '0x5ba1e12693dc8f9c48aad8770482f4739beed696'
+export const MULTICALL_ADDRESS = '0xcA11bde05977b3631167028862bE2a173976CA11'
 
 export const processor = new EvmBatchProcessor()
-    .setDataSource({
-        archive: lookupArchive('eth-mainnet'),
-        chain: 'https://rpc.ankr.com/eth',
+    // .setDataSource({
+    //     // archive: lookupArchive('eth-mainnet'),
+    //     chain: 'https://rpc.ankr.com/eth',
+    // })
+    .setRpcEndpoint({
+        url: 'https://rpc.ankr.com/polygon_zkevm/bea1b19b2d5ad332a01aa74dea339b1934cb2c60b0903082438a206bd3adc8f8',
+        rateLimit: 30
     })
     .setFinalityConfirmation(10)
     .setBlockRange({
-        from: 15_584_000,
+        from: 19_231_037,
     })
     .setFields({
         evmLog: {
